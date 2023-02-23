296aced09f61fc16190a302bffe1a32defa12e08
Shaun Munsey <s_munsey@live.com>
2023-02-23 22:27:28 +0000

    home page

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,18 @@
+import { Container } from "@mui/material";
+import React from "react";
+
+const Home = () => {
+  return (
+    <Container maxWidth="sm">
+      <h1>About Me</h1>
+      <p>Hi, I'm Shaun Munsey</p>
+      <p>
+        I'm an software developer, experienced in C#, JavaScript, Python, and
+        Java
+      </p>
+      <p>Welcome to my site!</p>
+    </Container>
+  );
+};
+
+export default Home;
