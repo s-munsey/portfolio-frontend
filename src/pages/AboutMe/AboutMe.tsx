edfaf8912b9dbe19278cd15726335e594b050831
Shaun Munsey <s_munsey@live.com>
2023-02-25 00:33:01 +0000

    login form no functionality

--- src/pages/AboutMe/AboutMe.tsx
+++ src/pages/AboutMe/AboutMe.tsx
@@ -1,16 +1,18 @@
-import { Container } from "@mui/material";
+import { Container, Paper } from "@mui/material";
 import React from "react";
 
 const Home = () => {
   return (
     <Container maxWidth="sm">
-      <h1>About Me</h1>
-      <p>Hi, I'm Shaun Munsey</p>
-      <p>
-        I'm an software developer, experienced in C#, JavaScript, Python, and
-        Java
-      </p>
-      <p>Welcome to my site!</p>
+      <Paper sx={{ marginTop: "10%", padding: "2em" }}>
+        <h1>About Me</h1>
+        <p>Hi, I'm Shaun Munsey</p>
+        <p>
+          I'm an software developer, experienced in C#, JavaScript, Python, and
+          Java
+        </p>
+        <p>Welcome to my site!</p>
+      </Paper>
     </Container>
   );
 };
--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,10 +1,44 @@
-import { Container } from "@mui/material";
+import {
+  Box,
+  Button,
+  Container,
+  FormControl,
+  Input,
+  InputLabel,
+  Paper,
+  TextField,
+} from "@mui/material";
 import React from "react";
 
 const Login = () => {
   return (
     <Container maxWidth="sm">
-      <h1>Login</h1>
+      <Paper sx={{ marginTop: "10%", padding: "2em" }}>
+        <form autoComplete="off">
+          <h2>Login</h2>
+          <TextField
+            label="Email"
+            required
+            variant="outlined"
+            color="secondary"
+            type="email"
+            sx={{ mb: 3 }}
+            fullWidth
+          />
+          <TextField
+            label="Password"
+            required
+            variant="outlined"
+            color="secondary"
+            type="password"
+            fullWidth
+            sx={{ mb: 3 }}
+          />
+          <Button variant="outlined" color="secondary" type="submit">
+            Login
+          </Button>
+        </form>
+      </Paper>
     </Container>
   );
 };
